572a0f9fe4adcaba6c42f5637a402d7d37aa5412
Sobirov S <sobiroff23@gmail.com>
2024-12-17 16:18:41 +0500

    added arrays

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="filter-component">
     <div class="box_filter">
-    <input
-      v-model="searchQuery"
-      type="text"
-      placeholder="Поиск..."
-      class="search-input"
-    />
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Поиск..."
+        class="search-input"
+      />
 
-    <select v-model="selectedCategory" class="category-select">
-      <option value="">Все категории</option>
-      <option
-      v-for="(category, index) in uniqueCategories"
-      :key="index"
-      :value="category"
-      >
-      {{ category }}
-    </option>
-</select>
-</div>
+      <select v-model="selectedCategory" class="category-select">
+        <option value="">Все категории</option>
+        <option
+          v-for="(category, index) in uniqueCategories"
+          :key="index"
+          :value="category"
+        >
+          {{ category }}
+        </option>
+      </select>
+    </div>
 
     <transition-group name="list" tag="ul" class="item-list">
       <li v-for="item in filteredItems" :key="item.id" class="item">
@@ -27,7 +27,7 @@
     </transition-group>
 
     <p v-if="filteredItems.length === 0" class="no-results">
-        Нет товаров, соответствующих вашим фильтрам.
+      Нет товаров, соответствующих вашим фильтрам.
     </p>
   </div>
 </template>
@@ -36,11 +36,19 @@
 import { ref, computed } from "vue";
 
 const items = [
-  { id: 1, name: "Яблоко", category: "Фрукты" },
-  { id: 2, name: "Морковь", category: "Овощи" },
-  { id: 3, name: "Банан", category: "Фрукты" },
-  { id: 4, name: "Брокколи", category: "Овощи" },
-  { id: 5, name: "Апельсин", category: "Фрукты" },
+  { id: 1, name: "Огурец", category: "Овощи" },
+  { id: 2, name: "Яблоко", category: "Фрукты" },
+  { id: 3, name: "Морковь", category: "Овощи" },
+  { id: 4, name: "Банан", category: "Фрукты" },
+  { id: 5, name: "Брокколи", category: "Овощи" },
+  { id: 6, name: "Киви", category: "Фрукты" },
+  { id: 7, name: "Картошка", category: "Овощи" },
+  { id: 8, name: "Мандарин", category: "Фрукты" },
+  { id: 9, name: "Тыква", category: "Овощи" },
+  { id: 10, name: "Арбуз", category: "Фрукты" },
+  { id: 11, name: "Лук", category: "Овощи" },
+  { id: 12, name: "Какос", category: "Фрукты" },
+  { id: 13, name: "Капуста", category: "Овощи" },
 ];
 
 const searchQuery = ref("");
